646358da4c5c9206904ebd3a973eac05b114039f
Maroof Afzal <maroofafzal92@gmail.com>
2025-02-23 15:56:05 +0500

    Add files via upload

---
+++ table6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the maximum number for tables: " ;
+	cin>>n;
+	int num[n];
+	cout<<"You Required Ouput is:\n";
+	for(int i=1;i<=10;i++){
+	for(int j=1;j<=n;j++){
+	cout<<j<<" * "<<i<<" = "<<j*i<<"\t  ";}
+	cout<<endl;
+	}
+}
+ 
